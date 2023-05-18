39d14c52ba78b7665e5f8862ec77231763597cad
lin <ichenzhl@qq.com>
2023-05-18 14:24:03 +0800

    fix1

--- main.py
+++ main.py
@@ -92,16 +92,15 @@ def init_prompts():
     初始化前置prompt，便于模型做 incontext learning。
     """
     class_list = list(set(class_examples.values()))
-    class_list.append(unknown)
     pre_history = [
         (
-            f'现在你是一个文本分类器，你需要按照要求将我给你的句子分类到：{class_list}类别中。不确定怎么分类的就分类到 {unknown} ',
+            f'现在你是一个文本分类器，你需要按照要求将我给你的句子分类到：[{class_list}]类别中。不确定怎么分类的就分类到 {unknown} ',
             f'好的。'
         )
     ]
 
     for exmpale,_type in class_examples.items():
-        pre_history.append((f'"{exmpale}" 是 {class_list} 里的什么类别？', _type))
+        pre_history.append((f'"{exmpale}" 是 [{",".join(class_list)}] 里的什么类别？'.replace("\'",""), _type))
     return {'class_list': class_list, 'pre_history': pre_history}
 
 
